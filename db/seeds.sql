6646680d78345507011698f59e033ca10c6def7f
Spencer-P-Brown <Spencer-P-Brown@github.com>
2017-07-26 18:28:05 -0500

    added code in all the files to get basic app functioning

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Plain Hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Plain Cheesburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger all the way", false);
